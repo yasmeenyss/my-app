bf24f66849fa10d0c017aacee57a56aab4f2dce1
yasmeenyss <yasmeenyss461@gmail.com>
2026-08-25 16:26:56 +0530

    Tailwindcss

---
+++ Tailwindcss/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+
+
+  return (
+    <>
+    <div className="align-center m-2 justify-center border">
+       <h1 class=" m-4  p-4 text-8xl font-bold  text-blue-600  underline">
+        Hello world!
+        </h1>
+        </div> 
+    </>
+  )
+}
+
+export default App
